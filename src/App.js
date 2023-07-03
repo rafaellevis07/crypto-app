020040906828fec72cd7e6fbe480bd32ecf50fe4
Rafael Garcia <rafaellevis07@gmail.com>
2023-07-03 15:46:36 -0700

    Routes Established

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import { ThemeProvider } from "./context/ThemeContext";
 import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+import SignIn from "./routes/SignIn";
+import SignUp from "./routes/SignUp";
+import Account from "./routes/Account";
 
 function App() {
   return (
     <ThemeProvider>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/account" element={<Account />} />
+      </Routes>
     </ThemeProvider>
   );
 }
