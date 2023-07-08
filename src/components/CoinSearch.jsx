3826385a708373d3ad5d8dae1eb4241e33aff6f6
Rafael Garcia <rafaellevis07@gmail.com>
2023-07-08 14:15:24 -0700

    update: Assign data to table at CoinSearch; install: sparkline package

--- src/components/CoinSearch.jsx
+++ src/components/CoinSearch.jsx
@@ -1,8 +1,62 @@
 import React from "react";
+import { AiOutlineStar } from "react-icons/ai";
+import { Sparklines, SparklinesLine } from "react-sparklines";
 
 const CoinSearch = ({ coins }) => {
-  console.log(coins);
-  return <div>CoinSearch</div>;
+  //   console.log(coins);
+  return (
+    <div>
+      <div>
+        <h1>Search Crypto</h1>
+        <form>
+          <input type="text" placeholder="Search a coin" />
+        </form>
+      </div>
+
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>#</th>
+            <th>Coin</th>
+            <th></th>
+            <th>Price</th>
+            <th>24h</th>
+            <th>24h Volume</th>
+            <th>Mkt</th>
+            <th>Lasr 7 Days</th>
+          </tr>
+        </thead>
+        <tbody>
+          {coins.map((coin) => (
+            <tr>
+              <td>
+                <AiOutlineStar />
+              </td>
+              <td>{coin.market_cap_rank}</td>
+              <td>
+                <div>
+                  <img src={coin.image} alt={coin.id} />
+                  {/* alt -> Dynamic alt tag to images */}
+                  <p>{coin.name}</p>
+                </div>
+              </td>
+              <td>{coin.symbol}</td>
+              <td>{coin.current_price}</td>
+              <td>{coin.price_change_percentage_24h}</td>
+              <td>{coin.total_volume}</td>
+              <td>{coin.market_cap}</td>
+              <td>
+                <Sparklines data={coin.sparkline_in_7d.price}>
+                  <SparklinesLine color="teal" />
+                </Sparklines>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default CoinSearch;
