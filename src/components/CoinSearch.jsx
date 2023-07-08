653211a03c2d03df3d7933fb9bc19f3437e06918
Rafael Garcia <rafaellevis07@gmail.com>
2023-07-08 14:34:11 -0700

    create: CoinItem and update API to full functionality no errors

--- src/components/CoinSearch.jsx
+++ src/components/CoinSearch.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { AiOutlineStar } from "react-icons/ai";
-import { Sparklines, SparklinesLine } from "react-sparklines";
+import CoinItem from "./CoinItem";
 
 const CoinSearch = ({ coins }) => {
   //   console.log(coins);
@@ -29,29 +28,7 @@ const CoinSearch = ({ coins }) => {
         </thead>
         <tbody>
           {coins.map((coin) => (
-            <tr>
-              <td>
-                <AiOutlineStar />
-              </td>
-              <td>{coin.market_cap_rank}</td>
-              <td>
-                <div>
-                  <img src={coin.image} alt={coin.id} />
-                  {/* alt -> Dynamic alt tag to images */}
-                  <p>{coin.name}</p>
-                </div>
-              </td>
-              <td>{coin.symbol}</td>
-              <td>{coin.current_price}</td>
-              <td>{coin.price_change_percentage_24h}</td>
-              <td>{coin.total_volume}</td>
-              <td>{coin.market_cap}</td>
-              <td>
-                <Sparklines data={coin.sparkline_in_7d.price}>
-                  <SparklinesLine color="teal" />
-                </Sparklines>
-              </td>
-            </tr>
+            <CoinItem key={coin.id} coin={coin} />
           ))}
         </tbody>
       </table>
