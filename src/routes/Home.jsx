317a3d1450d27977e58aada817e04020730f9f17
Rafael Garcia <rafaellevis07@gmail.com>
2023-07-10 12:11:26 -0700

    create: trending and style

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import CoinSearch from "../components/CoinSearch";
+import Trending from "../components/Trending";
 
 const Home = (props) => {
   return (
     <div>
       <CoinSearch coins={props.coins} />
+      <Trending />
     </div>
   );
 };
