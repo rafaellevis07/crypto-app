1a7f39e6deef50b81b0e6bcee89565a77ff5fcf2
Rafael Garcia <rafaellevis07@gmail.com>
2023-07-18 22:27:54 -0700

    fix: small fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,14 +27,14 @@ const Navbar = () => {
   return (
     <div className="rounded-div flex items-center justify-between h-20 font-bold">
       <Link to="/">
-        <h1 className="text-2xl">Cryptobase</h1>
+        <h1 className="text-2xl p-2">Cryptobase</h1>
       </Link>
       <div className="hidden md:block">
         <ThemeToggle />
       </div>
 
       {user?.email ? (
-        <div className="hidden md:block">
+        <div className="hidden md:block p-2">
           <Link to="/account" className="p-4">
             Account
           </Link>
